52c1bf68e4e5ab0bee63cd64282e4b10e577934b
Srushti Rathva <76732482+srushti1hub@users.noreply.github.com>
2022-06-06 13:10:52 +0530

    Create 1089.Duplicate-Zeros.cpp

---
+++ Leetcode/1089.Duplicate-Zeros.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        for(int i = 0; i<arr.size()-1; i++)
+        {
+           if(!arr[i])
+            {
+                for(int j = arr.size()-1; j>i; j--)
+                {
+                    arr[j] = arr[j-1];
+                }
+                i++; 
+            }
+        }
+    }
+};
