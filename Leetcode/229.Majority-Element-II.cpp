dd0bf0fcf71d4f8771bd2a7c3775f13271f71e5b
Srushti Rathva <76732482+srushti1hub@users.noreply.github.com>
2022-06-21 12:57:41 +0530

    Create 229.Majority-Element-II.cpp

---
+++ Leetcode/229.Majority-Element-II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int> Map;
+        vector<int> ans;
+        
+        for(int i=0;i<nums.size();i++)
+            Map[nums[i]]++;
+        
+        for(auto i: Map)
+        {
+            if(i.second>nums.size()/3)
+                ans.push_back(i.first);
+        }
+        
+        return ans;
+    }
+};
