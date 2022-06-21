b7dd7ba819c91b87c42b870da3f77c5e67fb498d
Srushti Rathva <76732482+srushti1hub@users.noreply.github.com>
2022-06-21 16:56:05 +0530

    Create contiguous_maxsum.cpp

---
+++ GFG/Arrays/contiguous_maxsum.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int a[], int n){
+        // Your code here
+        int max_sum = INT_MIN,curr_sum = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(curr_sum<0)
+               curr_sum = 0;
+               
+            curr_sum += a[i];
+            max_sum = max(max_sum,curr_sum);
+        }
+        return max_sum;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
