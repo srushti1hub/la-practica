f18f9fc5326538cbaa421121fcfdb984423f236a
Srushti Rathva <76732482+srushti1hub@users.noreply.github.com>
2022-07-05 14:05:52 +0530

    Create chocolate-distribution.cpp

---
+++ GFG/Arrays/Other/chocolate-distribution.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+    if(n==0 || m==0)
+       return 0;
+       
+    if(n<m)
+       return -1;
+    
+    std::sort(a.begin(),a.end());
+    long long diff = INT_MAX;
+    
+    for(long long i = 0; i+m-1<n; i++)
+    {
+        long long currdiff =a[i+m-1] - a[i];
+        diff = min(diff,currdiff);
+    }
+      return diff;
+    }   
+};
+
+// { Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
