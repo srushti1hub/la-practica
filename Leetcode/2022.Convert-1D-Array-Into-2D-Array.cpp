e7bafb360d6fc9ab9ffc7318618b69ae14d2d049
Srushti Rathva <76732482+srushti1hub@users.noreply.github.com>
2022-07-18 09:45:12 +0530

    Create 2022.Convert-1D-Array-Into-2D-Array.cpp

---
+++ Leetcode/2022.Convert-1D-Array-Into-2D-Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        vector<vector<int>> ans(m,vector<int>(n));
+        int k = 0;
+        
+        if( original.size() != m*n)
+            return {};
+        
+        for(int i=0; i<m; i++){
+           for(int j=0; j<n; j++){
+               ans[i][j] = original[k];
+               k++;
+           } 
+        }
+        return ans;
+    }
+};
