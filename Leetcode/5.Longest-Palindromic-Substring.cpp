2a55ea7d32abc67c133b2f65cab689a7ee0d3755
Srushti Rathva <76732482+srushti1hub@users.noreply.github.com>
2022-08-06 20:53:56 +0530

    Create 5.Longest-Palindromic-Substring.cpp

---
+++ Leetcode/5.Longest-Palindromic-Substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string get_pal(string s,int left,int right){
+     int n = s.length();
+     while(left>=0 && right<n){
+         if(s[left]!=s[right]){
+             break;
+         }
+         left--;
+         right++;
+     }
+     return s.substr(left+1,right-left-1);
+   }
+    string longestPalindrome(string s) {
+        int n=s.length();
+        if(s.empty()) return 0;
+        string longest;
+        
+        for(int i=0;i<n;i++){
+            string odd=get_pal(s,i,i);
+            if(odd.length()>longest.length())
+                longest=odd;
+        }
+        
+        for(int i=0;i<n;i++){
+            string even=get_pal(s,i,i+1);
+            if(even.length()>longest.length())
+                longest=even;
+        }
+        return longest;
+    }
+};
