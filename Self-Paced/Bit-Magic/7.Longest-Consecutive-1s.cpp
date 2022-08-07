9aa5e8e60430a8a248967d6eee6072ee09f7c858
Srushti Rathva <76732482+srushti1hub@users.noreply.github.com>
2022-08-07 12:46:08 +0530

    Create 7.Longest-Consecutive-1s.cpp

---
+++ Self-Paced/Bit-Magic/7.Longest-Consecutive-1s.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*  Function to calculate the longest consecutive ones
+*   N: given input to calculate the longest consecutive ones
+*/
+class Solution
+{
+    public:
+    int maxConsecutiveOnes(int N)
+    {
+        // code here
+        int Curr = 0, Max = INT_MIN;
+        for(int i=0; i<32; i++){
+            Max = max(Max,Curr);
+            if(N&(1<<i)) Curr++;
+            else Curr = 0;
+        }
+        return Max;
+    }
+};
+
+
+// { Driver Code Starts.
+
+// Driver Code
+int main() {
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n;
+		cin>>n;//input n
+		Solution obj;
+		//calling maxConsecutiveOnes() function
+		cout<<obj.maxConsecutiveOnes(n)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
